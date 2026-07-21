8c6dd575f2da331ac8d771d2d7cd9afe5a1a25ad
itsLKX <kahxiong00@gmail.com>
2026-07-21 20:13:52 +0800

    feat(models): add category label/emoji/color extension mapping

--- expiry_vault/lib/models/item_category.dart
+++ expiry_vault/lib/models/item_category.dart
@@ -1,4 +1,6 @@
 import 'package:hive/hive.dart';
+import 'package:flutter/material.dart';
+import '../theme/app_colors.dart';
 
 part 'item_category.g.dart';
 
@@ -16,4 +18,46 @@ enum ItemCategory {
   cosmetics,
   @HiveField(5)
   frozen,
+}
+
+extension ItemCategoryX on ItemCategory {
+  String get label {
+    switch (this) {
+      case ItemCategory.produce: return 'Produce';
+      case ItemCategory.dairy: return 'Dairy & Fridge';
+      case ItemCategory.pantry: return 'Pantry';
+      case ItemCategory.medicine: return 'Medicine';
+      case ItemCategory.cosmetics: return 'Cosmetics';
+      case ItemCategory.frozen: return 'Frozen';
+    }
+  }
+
+  String get emoji {
+    switch (this) {
+      case ItemCategory.produce: return '🥬';
+      case ItemCategory.dairy: return '🥛';
+      case ItemCategory.pantry: return '🥫';
+      case ItemCategory.medicine: return '💊';
+      case ItemCategory.cosmetics: return '🧴';
+      case ItemCategory.frozen: return '🧊';
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case ItemCategory.produce: return AppColors.freshGreen;
+      case ItemCategory.dairy: return AppColors.vaultBlue;
+      case ItemCategory.pantry: return AppColors.peach;
+      case ItemCategory.medicine: return AppColors.grapePurple;
+      case ItemCategory.cosmetics: return AppColors.tomatoRed;
+      case ItemCategory.frozen: return AppColors.sunshineYellow;
+    }
+  }
+
+  Color get onColor {
+    switch (this) {
+      case ItemCategory.frozen: return AppColors.charcoal;
+      default: return Colors.white;
+    }
+  }
 }
